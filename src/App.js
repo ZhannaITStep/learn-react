115be60ed3d4f827b0130ae1d53d766eaadcbb3e
Zhanna <hgdsajkh@.com>
2024-06-06 21:22:04 +0300

    props2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,36 @@ import React from "react";
 import { data } from "./data"
 import { CardItem } from "./CardItem";
 
-
 export const App = () => {
-    const [person] = data;
-    
+    const [persons, setPersons] = useState(data);
+    // TODO дописать логику удаления и укоротить код
+    const onDeletePerson = (personId) => {
+        setPersons((prevPersons) => {
+            return prevPersons.filter((person) => {
+                if (person.id !== personId) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        });
+    }
+
+    // const deleteItem = (index) => {
+    //     const updatedItems = [...items];
+    //     updatedItems.splice(index, 1);
+    //     setItems(updatedItems);
+    //   };
+
     return <>
         <div>hello world!</div>
-        <CardItem name={person.name} age={person.age} phone={person.phone} />
+        {/* <CardItem name={name} age={age} phone={phone} /> */}
+        { persons.map((person) => <CardItem {...person} key='person.id' />) }
+        {/* {items.map((person, index) => (
+        <div key={person.id}>
+          <CardItem {...person} />
+        </div>
+      ))} */}
     </>
+};
 
-}
--- src/CardItem.js
+++ src/CardItem.js
@@ -2,12 +2,13 @@ import React from "react";
 
 
 export const CardItem = ({name, age, phone}) => {
-
     return (
         <div>
+            <hr/>
             <h2>{name}</h2>
             <p>{age}</p>
             <p>{phone}</p>
+            <button onClick={() => {}}>delete</button>
         </div>
     )
 };

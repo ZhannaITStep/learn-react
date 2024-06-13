a04e93974f795ce41142d70a8f71f4ac4045b0b5
Zhanna <hgdsajkh@.com>
2024-06-13 19:39:21 +0300

    mount2

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,20 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
 
 export const Child = () => {
-    const [,rerender] = useState({});
+    const [, rerender] = useState({});
+    const [posts, setPosts] = useState([]);
 
-    console.log("RENDER CHILD!")
+    // setInterval()
+
+    // clearInterval()
+
+
+console.log("RENDER CHILD!");
 
     return <div>
             <h2>CHILD!</h2>
+            {/* <button onClick={() => setCounter(counter+1)}>+</button>
+            <button onClick={() => setCounter(counter-1)}>-</button> */}
             <button onClick={() => rerender({})}>rerender child</button>
         </div>
 };

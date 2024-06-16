3f457c878049b815e6dbe788c30647b4ebba74aa
Zhanna <hgdsajkh@.com>
2024-06-16 18:02:23 +0300

    memo1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 
 
 export const App = () => {
+    const [posts, setPosts] = useEffect([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        
+    })
+
     return <div>hello world!</div>
 }
